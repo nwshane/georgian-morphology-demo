73d9f423939b94c49de35fc9e2ad6d2c13db723d
nwshane <django09@gmail.com>
2016-03-08 11:12:25 +0400

    Added right and down carets to გავაკეთე page verb blocks to indicate collapsable and expandable.

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,10 @@
-$('.js-expand-collapse').click(function() {
-  $(this).toggleClass('is-collapsed');
+$('.js-expand-collapse-siblings').click(function() {
+  $(this)
+    .find('i')
+    .toggleClass('fa-caret-right')
+    .toggleClass('fa-caret-down');
+
+  $(this).siblings('.is-collapsed,.js-collapsable').each(function() {
+    $(this).toggleClass('is-collapsed').toggleClass('js-collapsable');
+  });
 });
